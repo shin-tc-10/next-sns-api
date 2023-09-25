76fddf2733816ee51a17b7dbfe82009c522c2dae
shin-tc-10 <shin.nkt.09@gmail.com>
2023-09-25 23:31:54 +0900

    誤字修正

--- routers/users.js
+++ routers/users.js
@@ -24,7 +24,7 @@ router.get("/find", isAuthenticated, async (req, res) => {
       },
     });
   } catch (err) {
-    res.status(500).json({ error: erros.message });
+    res.status(500).json({ error: err.message });
   }
 });
 
