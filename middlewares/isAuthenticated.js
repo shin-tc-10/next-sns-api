5ca0b834e0108bc9ef6b8984c13458a4f4246389
shin-tc-10 <shin.nkt.09@gmail.com>
2023-09-30 10:34:42 +0900

    微修正

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -9,13 +9,13 @@ function isAuthenticated(req, res, next) {
 
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
     if (err) {
-      return res.status(401).json({ message: "権限がありません、" });
+      return res.status(401).json({ message: "権限がありません。" });
     }
 
     req.userId = decoded.id;
 
     next();
-  });
+  })
 }
 
 module.exports = isAuthenticated;
